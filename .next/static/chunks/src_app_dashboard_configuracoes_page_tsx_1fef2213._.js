71b91ed63a728eb9fcb3db34cf7e59e2ee51b88b
LeandroMoraes00 <leandrovectra2.2@gmail.com>
2025-06-18 13:01:06 -0400

    Add .next/static/chunks/src_app_dashboard_configuracoes_page_tsx_1fef2213._.js 1750266115131

---
+++ .next/static/chunks/src_app_dashboard_configuracoes_page_tsx_1fef2213._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_18213b24._.js"
+],
+    source: "dynamic"
+});
